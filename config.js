01d19087501fd6442e7dff33a2e4fce8b40658cb
muratanel <muratanel7@gmail.com>
2026-05-16 22:34:04 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // Файл глобальных настроек (config.js)
 
 // 1. Вставьте сюда ваш актуальный ENCODED текст для Google Apps Script
-const _0x1a8f = "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J4S3FvQ29DZ0VZSldwdnpNMENXSGZLdTdlblNrR3llVTVWLWRBd3lsQW1CZEVFaTVYaWZDaDVyQ1JjOGlUczZvcTEvZXhlYw=="; 
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzeaUQamuGOih-r--nSvEEkidNREnllZdbL27oc_c0DTGEhHZJe4-L22Xw2-Gd9HC9K/exec"; 
 
 // 2. Ссылка на вебхук Make.com
-const _0x5bc2 = "aHR0cHM6Ly9ob29rLmV1MS5tYWtlLmNvbS85dWQyeWV0dHIzbDZrdG81bmx0cXR0am9jZmI2cDZvZw==";
+// const _0x5bc2 = "aHR0cHM6Ly9ob29rLmV1MS5tYWtlLmNvbS85dWQyeWV0dHIzbDZrdG81bmx0cXR0am9jZmI2cDZvZw==";
 
 // Расшифровка переменных (эти переменные станут доступны всем HTML-файлам)
-const APPS_SCRIPT_URL = window.atob(_0x1a8f);
-const MAKE_WEBHOOK_URL = window.atob(_0x5bc2);
+// const APPS_SCRIPT_URL = window.atob(_0x1a8f);
+// const MAKE_WEBHOOK_URL = window.atob(_0x5bc2);
