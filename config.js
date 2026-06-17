754507aa4bb40bd9d0a65404ac64fd6d7610047f
web-kassa <marcus.ben19999@gmail.com>
2026-06-17 22:42:42 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Файл глобальных настроек (config.js)
+
+// 1. Вставьте сюда ваш актуальную ссылку для Google Apps Script
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw9j0r3gQJRgZ2N50-NhNeFZBIBJP5dubSEdnYk2FllU-0hEz1YWQdKt6DykctRyMP5/exec"; 
+
---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = '062'; // <--- ВОТ ТУТ ПИШЕТСЯ ВЕРСИЯ, КОТОРУЮ ПОКАЖЕТ КАССА
+
+const ASSETS = [
+  './',
+  './index.html',
+  './config.js',
+  './manifest.json',
+  './icon.png' // <--- Вот она, разгадка! Мы указали правильное имя.
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting(); // Заставляем обновиться без ожидания
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim()); // Мгновенно берем кассу под контроль!
+  
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', e => {
+    // --- НАЧАЛО: СПАСАЕМ БАЗУ ДАННЫХ ---
+    // Если запрос идет к Google (наша база данных) - не перехватываем его!
+    if (e.request.url.includes('script.google.com') || e.request.url.includes('script.googleusercontent.com')) {
+        return; // Браузер сам скачает данные в обход Service Worker'а
+    }
+    // --- КОНЕЦ ---
+  
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+// ТОТ САМЫЙ БЛОК, КОТОРЫЙ ОТВЕЧАЕТ НА ВОПРОС КАССЫ "КАКАЯ ВЕРСИЯ?"
+self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'GET_VERSION') {
+        event.ports[0].postMessage({ version: CACHE_NAME });
+    }
+});
