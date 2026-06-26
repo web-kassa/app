22c5a9c22836ace82f32a1604fff6e2ee24132e3
web-kassa <marcus.ben19999@gmail.com>
2026-06-26 23:52:14 +0500

    Update config.js to comment out CLIENT_API_KEY
    Commented out the CLIENT_API_KEY variable for security.

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 
 // 1. Вставьте сюда ваш актуальную ссылку для Google Apps Script
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw9j0r3gQJRgZ2N50-NhNeFZBIBJP5dubSEdnYk2FllU-0hEz1YWQdKt6DykctRyMP5/exec"; 
-const CLIENT_API_KEY = "TEST_KEY_123";
+// const CLIENT_API_KEY = "TEST_KEY_123";
