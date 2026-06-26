60885e916b3033a3dfdde0e187b49a18748b30c5
web-kassa <marcus.ben19999@gmail.com>
2026-06-27 01:11:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // Файл глобальных настроек (config.js)
 
 // 1. Вставьте сюда ваш актуальную ссылку для Google Apps Script
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw9j0r3gQJRgZ2N50-NhNeFZBIBJP5dubSEdnYk2FllU-0hEz1YWQdKt6DykctRyMP5/exec"; 
-// const CLIENT_API_KEY = "TEST_KEY_123";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx9sjMg5Rny73gtxpfjHQqOegIHBnTWrMV2wVSpUkjUnQIOCvzb4sI9zSH3XxEZ5Xc/exec"; 
